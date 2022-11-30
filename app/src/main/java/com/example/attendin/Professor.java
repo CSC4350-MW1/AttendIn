6e60189d6bb73607967168f61886e5ddb0865dcb
Vincent Kim <vkimroyal@gmail.com>
2022-11-29 20:37:20 -0500

    Add all merged branches to master

---
+++ app/src/main/java/com/example/attendin/Professor.java
@@ -0,0 +1,14 @@
+package com.example.attendin;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Professor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_professor);
+    }
+}
